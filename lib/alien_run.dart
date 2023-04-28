015a0cce647d84350f384f12d4f5697c3b559c54
Snikyy4 <lequertierthibaud@gmail.com>
2023-04-28 16:27:44 +0200

    alien run marche niquel

--- lib/alien_run.dart
+++ lib/alien_run.dart
@@ -1,10 +1,10 @@
 import 'dart:async';
 import 'dart:math';
+import 'package:flutter/material.dart';
 import 'package:projet_mob/challenge_screen.dart';
 import 'package:sqflite/sqflite.dart';
 
 import 'main.dart';
-import 'package:flutter/material.dart';
 
 class AlienRun extends StatefulWidget {
   @override
@@ -12,35 +12,60 @@ class AlienRun extends StatefulWidget {
 }
 
 class _AlienRunState extends State<AlienRun> {
+  final double _blockSize = 50.0;
   double _playerX = 0.0;
   double _playerY = 0.0;
-  late Timer _timer;
-  double _wallY = 0.0;
-  double _holeX = 0.0;
+  double _blockY = 0.0;
+  double _blockX = 0.0;
   int _score = 0;
-  Random _random = Random();
+  bool _end = false;
+  Timer? _timer;
+  int _blockSpeedMultiplier = 1;
+
+  DateTime startTime = DateTime.now();
+  int timeRemaining = 30;
 
   @override
   void initState() {
     super.initState();
-    _timer = Timer.periodic(const Duration(milliseconds: 10), (timer) {
+
+    WidgetsBinding.instance!.addPostFrameCallback((_) {
+      _playerX = MediaQuery.of(context).size.width / 2 -
+          25; // 25 = half the width of the image
+      _playerY = MediaQuery.of(context).size.height - 110;
+    });
+
+    // Start the game loop
+    _timer = Timer.periodic(const Duration(milliseconds: 5), (timer) {
       setState(() {
-        _wallY += 1.0;
-        if (_wallY >= MediaQuery.of(context).size.height) {
-          _wallY = 0.0;
-          _holeX = _random.nextDouble() * MediaQuery.of(context).size.width;
+        // Move the block down the screen
+        _blockY += 1 * _blockSpeedMultiplier;
+
+        // Check if the block has reached the bottom of the screen
+        if (_blockY > MediaQuery.of(context).size.height) {
+          spawnBlock();
         }
-        if (_playerY <= _wallY + 50.0 &&
-            _playerX >= _holeX - 50.0 &&
-            _playerX <= _holeX + 50.0) {
+
+        // Check if the player has collided with the block
+        if (_playerX + 25 >= _blockX &&
+            _playerX + 25 <= _blockX + _blockSize &&
+            _playerY + 50 >= _blockY &&
+            _playerY <= _blockY + _blockSize) {
           _score++;
+          spawnBlock();
         }
-        if (_playerY >= _wallY - 50.0 &&
-            _playerX >= _holeX - 50.0 &&
-            _playerX <= _holeX + 50.0) {
-          _timer.cancel();
-          scoreAlienRun = _score;
-          updateScore(scoreAlienRun);
+
+        // Check if the game has ended
+        if (DateTime.now().difference(startTime).inSeconds >= 30) {
+          _end = true;
+          timer.cancel();
+
+          if (_score > scoreAlienRun || _score == -1) {
+            // Check if it's the first time playing or if the new score is better than the one in the database
+            scoreAlienRun = _score;
+            updateScore(scoreAlienRun);
+          }
+
           showDialog(
             context: context,
             builder: (BuildContext context) {
@@ -71,42 +96,123 @@ class _AlienRunState extends State<AlienRun> {
         }
       });
     });
+
+    Timer.periodic(const Duration(seconds: 1), (timer) {
+      if (!_end) {
+        setState(() {
+          if (timeRemaining > 0) {
+            timeRemaining--;
+          }
+        });
+      } else {
+        timer.cancel();
+      }
+    });
+
+    // Increase the speed of the blocks every 3 seconds
+    Timer.periodic(const Duration(seconds: 4), (timer) {
+      if (!_end) {
+        setState(() {
+          _blockSpeedMultiplier++;
+        });
+      } else {
+        timer.cancel();
+      }
+    });
+  }
+
+  void spawnBlock() {
+    _blockX = Random()
+        .nextInt(MediaQuery.of(context).size.width.toInt() - _blockSize.toInt())
+        .toDouble();
+    _blockY = -_blockSize;
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   title: Text('My Game'),
-      // ),
-      body: GestureDetector(
-        onPanUpdate: (DragUpdateDetails details) {
-          setState(() {
-            _playerX += details.delta.dx;
-          });
-        },
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('lib/assets/alienrun.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
         child: Stack(
           children: [
             Positioned(
               left: _playerX,
               top: _playerY,
-              child: Image.asset('lib/assets/vaisseau.png'),
+              child: GestureDetector(
+                onHorizontalDragUpdate: (DragUpdateDetails details) {
+                  setState(() {
+                    _playerX += details.delta.dx;
+                  });
+                },
+                child: Image.asset(
+                  'lib/assets/vaisseau.png',
+                  width: 75,
+                  height: 75,
+                ),
+              ),
             ),
             Positioned(
-              left: _holeX,
-              top: _wallY,
-              child: Image.asset('lib/assets/trou_noir.png'),
+              left: _blockX,
+              top: _blockY,
+              child: Image.asset(
+                'lib/assets/trou_noir.png',
+                width: 50,
+                height: 50,
+              ),
+            ),
+            Align(
+              alignment: Alignment.topCenter,
+              child: Column(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(16.0),
+                    decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.5),
+                    ),
+                    child: Text(
+                      'Temps restant: $timeRemaining secondes',
+                      style: const TextStyle(
+                        fontFamily: 'Roboto',
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(16.0),
+                    decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.5),
+                    ),
+                    child: Text(
+                      'Score: $_score',
+                      style: const TextStyle(
+                        fontFamily: 'Roboto',
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  
+                ],
+              ),
             ),
           ],
         ),
       ),
     );
-  }
-
-  @override
-  void dispose() {
-    _timer.cancel();
-    super.dispose();
   }
 }
 
